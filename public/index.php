385bac68704929da3af4e206c981773cc7e77a92
Adam Hughes <adam@tokyo.uk>
2017-03-03 14:49:58 +0000

    first commit

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js ie ie6" lang="en-GB"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie ie7" lang="en-GB"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js ie ie8" lang="en-GB"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie ie9" lang="en-GB"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en-GB"> <!--<![endif]-->
+<head>
+  <title>A-Frame Lab</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link rel="stylesheet" href="../../../assets/build/css/styles.css">
+</head>
+
+<body>
+
+  <a-scene>
+    <a-sky src="images/dining-room-skybox.JPG" rotation="0 -130 0"></a-sky>
+  </a-scene>
+
+<script src="../../../assets/src/js/libs/modernizr-custom.js"></script>
+<script src="../../../assets/src/js/libs/jquery-1.12.4.min.js"></script>
+<script src="../../../assets/src/js/libs/enquire.min.js"></script>
+<script src="https://aframe.io/releases/0.5.0/aframe.js"></script>
+<script src="../../../assets/src/js/app.js"></script>
+</body>
+</html>
